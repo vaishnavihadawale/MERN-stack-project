da85787eab494218b27077c07cdbbe7c8edc91b7
vaishnavihadawale <vaishnavi.hadawale1@gmail.com>
2024-02-20 23:08:17 +0530

    Components are created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
-import './App.css';
-
+import "./App.css";
+import { Home } from "./component/Home/Home";
+import { Navbar } from "./component/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import { Login } from "./component/Login/Login";
+import { Footer } from "./component/Footer/Footer";
 function App() {
   return (
-    <></>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer></Footer>
+    </div>
   );
 }
 
