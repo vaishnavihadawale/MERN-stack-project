35abea24717683e518c6c5f83f2afc61ef14185d
vaishnavihadawale <vaishnavi.hadawale1@gmail.com>
2024-04-01 14:59:28 +0530

    Navbar Created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import { Layout } from "./components/Layout";
-
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <div>
-      <Layout></Layout>
+   
+<Header></Header>
     </div>
   );
 }
